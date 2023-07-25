ac1308d021292125007ab9221d18f9bdc2e1ef7e
krgarav <krgarav@gmail.com>
2023-07-25 12:14:20 +0530

    Updated deletion method with sequelize transaction

--- Backend/Controllers/expense.js
+++ Backend/Controllers/expense.js
@@ -1,16 +1,25 @@
+
 const Expense = require("../Models/expense");
 const User = require("../Models/user");
+const sequelize = require("../Util/database");
 
-exports.postExpense = (req, res, next) => {
+exports.postExpense = async (req, res, next) => {
     const { quantity, description, category } = req.body;
 
+    const t = await sequelize.transaction();
 
     const postData = async () => {
-        const totalAmount = req.user.totalExpense += +quantity;
-        await req.user.update({ totalExpense: totalAmount })
-
-        await req.user.createExpense({ amount: quantity, description, category });
-        res.status(200).json({ message: "Expense Created" });
+        try {
+            const totalAmount = req.user.totalExpense += +quantity;
+            await req.user.createExpense({ amount: quantity, description, category }, { transaction: t });
+            await req.user.update({ totalExpense: totalAmount }, { transaction: t })
+            await t.commit();
+            res.status(200).json({ message: "Expense Created" });
+        } catch (err) {
+            await t.rollback();
+            res.status(500).json({ err: err })
+            console.log(err);
+        }
     }
     postData();
 };
@@ -27,16 +36,18 @@ exports.deleteExpense = (req, res, next) => {
     const expenseId = +req.params.expenseId;
 
     const deleteExpense = async () => {
-        try {
-            const expenses = await Expense.findAll({ where: { userId: req.user.id } });
-            await expenses.forEach(expense => {
-                if (expense.id === expenseId) {
-                    expense.destroy();
-                }
+        const t = await sequelize.transaction();
 
-            });
-            res.status(200).json({ message: "Expense deleted" })
+        try {
+            const toBeDeletedExpense = await Expense.findOne({ where: { userId: req.user.id, id: expenseId } }, { transaction: t });
+            const amount = req.user.totalExpense - +toBeDeletedExpense.amount;
+            await req.user.update({ totalExpense: amount }, { transaction: t });
+            await toBeDeletedExpense.destroy();
+            await t.commit();
+            res.status(200).json({ message: "Expense deleted" });
         } catch (err) {
+            await t.rollback();
+            res.status(500).json({ err: err });
             console.log(err)
         }
     }
